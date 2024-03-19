de4ec08f48e5eb97ac0a13c36845aacc5e564253
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-03-19 14:07:11 +0100

    dotenv added

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,9 @@
+import dotenv from dotenv;
+
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://root:root@mern-test.akbbnde.mongodb.net/?retryWrites=true&w=majority&appName=Mern-test";
+  "mongodb+srv://root:" + process.env.DB_PASSWORD + "@lia-event.6e7zew3.mongodb.net/?retryWrites=true&w=majority&appName=LIA-EVENT";
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
