e86d910f06f5a7b7882c6f06f2fb0bf870e1bda4
AntonBeYt <143318322+AntonBeYt@users.noreply.github.com>
2024-03-26 13:08:04 +0100

    theme and beginning of landing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,14 +4,39 @@ import { useCookies } from "react-cookie";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import Header from "../components/Header.jsx";
+import arrowRU from "../assets/icons/arrow-right-up.svg";
+import asterisk from "../assets/icons/asterisk-black.svg";
 
 const Home = () => {
   const navigate = useNavigate();
+  const handleLIA = () => {
+    navigate("/companies");
+  };
 
   return (
     <>
       <div className="home_page">
         <Header></Header>
+        <button
+          className="p-4 text-4xl font-normal border-solid border-b-2 border-black w-full flex flex-row justify-between align-baseline"
+          onClick={handleLIA}
+        >
+          VILKA KOMMER?
+          <img src={arrowRU} alt="arrow right up" />
+        </button>
+        <span className="p-4 text-8xl flex flex-row justify-between">
+          LIA &
+          <img src={asterisk} alt="asterisk icon" />
+        </span>
+        <span className="p-4 text-8xl font-bold text-right flex flex-row justify-end">
+          YRGO
+        </span>
+        <span>
+          <div>
+            <div>24 APRIL</div>
+            <div>MEETUP</div>
+          </div>
+        </span>
         <h1>THIS IS A HOME PAGE EVERYONE CAN ACCESS</h1>
       </div>
       <ToastContainer />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#333333",
+      red: "#F52A3B",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
