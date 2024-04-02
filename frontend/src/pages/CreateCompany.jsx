4ff0230bcb9b04a56efc98f466425de37ca5d231
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-04-02 13:55:32 +0200

    create company
    almost done, waiting for design

--- frontend/src/pages/CreateCompany.jsx
+++ frontend/src/pages/CreateCompany.jsx
@@ -7,6 +7,8 @@ import Header from "../components/Header";
 import AmountInput from "../components/AmountInput";
 
 const CreateCompany = () => {
+  const [dragging, setDragging] = useState(false);
+  const [fileName, setFileName] = useState("");
   const [cookies, removeCookie] = useCookies(["jwt"]);
   const [logotype, setLogotype] = useState(null);
   const [username, setUsername] = useState("");
@@ -71,7 +73,11 @@ const CreateCompany = () => {
   };
 
   const handleFileChange = (e) => {
-    setLogotype(e.target.files[0]);
+    const file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+      setLogotype(file);
+    }
   };
 
   const autoResize = (e) => {
@@ -150,16 +156,67 @@ const CreateCompany = () => {
           </h4>
           <button onClick={handleLogout}>LOGOUT</button>
         </div>
-        <h1 className="text-3xl my-4">Företagsprofil</h1>
+        <h1 className="my-4 text-6xl font-light md:text-center">
+          Företagsprofil
+        </h1>
 
-        <div className="flex flex-col border-sky-400 w-full px-4 mx-auto">
-          <div className="my-4">
-            <label className="text-xl mr-4 text-gray-500">Logotype</label>
-            <input
-              type="file"
-              onChange={handleFileChange}
-              className="form-input"
-            />
+        <div className="flex flex-col border-sky-400 w-full px-4 md:px-48 mx-auto">
+          <label className="text-xl mr-4 text-gray-500">Logotyp</label>
+          <div
+            className="bg-gray-50 text-center px-4 rounded w-80 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-400 border-dashed mx-auto font-[sans-serif]"
+            onDragOver={(e) => {
+              e.preventDefault();
+              setDragging(true);
+            }}
+            onDragLeave={() => {
+              setDragging(false);
+            }}
+            onDrop={(e) => {
+              e.preventDefault();
+              setDragging(false);
+              handleFileChange(e);
+            }}
+          >
+            {" "}
+            <div className="py-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 mb-2 fill-gray-600 inline-block"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
+                  data-original="#000000"
+                />
+                <path
+                  d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
+                  data-original="#000000"
+                />
+              </svg>
+              <h4 className="text-base font-semibold text-gray-600">
+                Drag and drop files here
+              </h4>
+            </div>
+            <hr className="w-full border-gray-400 my-2" />
+            <div className="py-6">
+              <input
+                type="file"
+                id="uploadFile1"
+                className="hidden"
+                onChange={handleFileChange}
+              />
+              <label
+                htmlFor="uploadFile1"
+                className="block px-6 py-2.5 rounded text-gray-600 text-sm tracking-wider font-semibold border-none outline-none bg-gray-200 hover:bg-gray-100"
+              >
+                Browse Files
+              </label>
+              <p className="text-xs text-gray-400 mt-4">
+                {fileName
+                  ? `Selected file: ${fileName}`
+                  : "PNG, JPG, SVG, WEBP, and GIF are allowed."}
+              </p>
+            </div>
           </div>
 
           <div className="my-4">
@@ -261,11 +318,17 @@ const CreateCompany = () => {
           </div>
 
           <button
-            className="bg-red text-white font-bold text-xl flex justify-center align-middle rounded-3xl w-[calc(100vw-32px)] my-12 p-3"
+            className="bg-red text-white font-bold text-xl flex justify-center align-middle rounded-3xl mb-5 p-3"
             onClick={handleUploadLogotype}
             disabled={loading}
           >
             Save
+          </button>
+          <button
+            className=" border-[1px] font-bold text-xl flex justify-center align-middle rounded-3xl mb-10 p-3"
+            onClick={() => navigate("/profile")}
+          >
+            Return
           </button>
         </div>
       </div>
