918719a7efb0ea9999d4b3eb0b0fadec93279843
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-04-03 15:30:10 +0200

    in progress

--- frontend/src/components/Company.jsx
+++ frontend/src/components/Company.jsx
@@ -1,6 +1,9 @@
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import HorizontalLine from "./HorizontalLine";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Company = () => {
   const { id } = useParams();
@@ -27,71 +30,91 @@ const Company = () => {
   }
 
   return (
-    <div className="p-4">
-      <Link to="/profile" className="p-2 bg-sky-300 m-8">
-        Go to Profile
-      </Link>
-      <div className="flex flex-col border-sky-400 rounded-xl w-3/4 p-4 mx-auto">
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Logotype</label>
-          {company.logotype && company.logotype.trim() && (
-            <>
-              <img src={company.logotype} alt="Company logotype" />
-            </>
-          )}
-        </div>
-        <div className="flex flex-row">
-          <div className=" ">
+    <>
+      <div className="relative min-h-screen overflow-x-clip">
+        <Header></Header>
+
+        <div className="">
+          <Link to="/profile" className="p-2 bg-sky-300 m-8">
+            Go to Profile
+          </Link>
+          <div className="flex flex-col border-sky-400 rounded-xl w-3/4 ">
             <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">Company Name</label>
-              <div className=" px-4 py-2 w-full">{company.name}</div>
+              <label className="text-xl mr-4 text-gray-500">Logotype</label>
+              {company.logotype && company.logotype.trim() && (
+                <>
+                  <img src={company.logotype} alt="Company logotype" />
+                </>
+              )}
             </div>
-            <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">About Us</label>
-              <div className=" px-4 py-2 w-full">{company.about}</div>
-            </div>
-            <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">Contact</label>
-              <div className=" px-4 py-2 w-full">{company.contact}</div>
-            </div>
-            <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">Role</label>
-              <div className=" px-4 py-2 w-full">{company.role}</div>
-            </div>
-            <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">Amount</label>
-              <div className=" px-4 py-2 w-full">{company.amount}</div>
-            </div>
-            <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">Location</label>
-              <div className=" px-4 py-2 w-full">{company.location}</div>
-            </div>
-            <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">Tools</label>
-              <ul className=" px-4 py-2 w-full">
-                {company.tools.map((tool, index) => (
-                  <li key={index}>{tool}</li>
-                ))}
-              </ul>
-            </div>
-            <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">URL</label>
-              <div className=" px-4 py-2 w-full">{company.url}</div>
-            </div>
-          </div>
-          <div>
-            <div className="my-4">
-              <label className="text-xl mr-4 text-gray-500">
-                Task Description
-              </label>
-              <div className=" px-4 py-2 w-full">
-                {company.task_description}
+            <div className="flex flex-col">
+              <div className=" ">
+                <HorizontalLine></HorizontalLine>
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500"></label>
+                  <div className=" px-4 py-2 w-full">{company.name}</div>
+                </div>
+
+                <HorizontalLine></HorizontalLine>
+
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">Roll</label>
+                  <div className=" px-4 py-2 w-full">{company.role}</div>
+                </div>
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">Plats</label>
+                  <div className=" px-4 py-2 w-full">{company.location}</div>
+                </div>
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">
+                    Lia-platser
+                  </label>
+                  <div className=" px-4 py-2 w-full">{company.amount}</div>
+                </div>
+              </div>
+              <div>
+                <HorizontalLine></HorizontalLine>
+
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">About Us</label>
+                  <div className=" px-4 py-2 w-full">{company.about}</div>
+                </div>
+
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">Tools</label>
+                  <ul className=" px-4 py-2 w-full">
+                    {company.tools.map((tool, index) => (
+                      <li key={index}>{tool}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">
+                    Task Description
+                  </label>
+
+                  <div className=" px-4 py-2 w-full">
+                    {company.task_description}
+                  </div>
+                </div>
+              </div>
+              <div>
+                <HorizontalLine></HorizontalLine>
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">Contact</label>
+                  <div className=" px-4 py-2 w-full">{company.contact}</div>
+                </div>
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">URL</label>
+                  <div className=" px-4 py-2 w-full">{company.url}</div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <Footer></Footer>
       </div>
-    </div>
+    </>
   );
 };
 
