d04c77b4f875f677e10b03246b32f91bfe615e66
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-04-12 13:16:41 +0200

    working

---
+++ frontend/src/components/password/ResetPasswordForm.jsx
@@ -0,0 +1,50 @@
+// ResetPassword.js
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const ResetPassword = ({ email }) => {
+  const [otp, setOtp] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5555/resetPassword", {
+        email,
+        otp,
+        password,
+      });
+      toast.success("Password reset successful.");
+      // Here you might want to redirect the user or give further instructions
+    } catch (error) {
+      toast.error(error.response?.data?.message || "An error occurred.");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="otp">OTP:</label>
+        <input
+          id="otp"
+          type="text"
+          value={otp}
+          onChange={(e) => setOtp(e.target.value)}
+          required
+        />
+        <label htmlFor="newPassword">New Password:</label>
+        <input
+          id="newPassword"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Reset Password</button>
+      </form>
+    </div>
+  );
+};
+
+export default ResetPassword;
--- frontend/src/pages/Password.jsx
+++ frontend/src/pages/Password.jsx
@@ -1,21 +1,37 @@
-import react from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// PasswordPage.js
+import React, { useState } from "react";
+import RequestOTP from "../components/password/RequestOTP";
+import ResetPassword from "../components/password/ResetPasswordForm";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Password = () => {
+const PasswordPage = () => {
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+
+  const handleEmailSubmit = (email) => {
+    setUserEmail(email);
+    setEmailSubmitted(true);
+  };
+
   return (
     <>
       <div className="overflow-x-clip relative">
-        <Header></Header>
-        <div className="h-screen">
+        <Header />
+        <div className="h-screen flex justify-center items-center">
           <ToastContainer />
+          {!emailSubmitted ? (
+            <RequestOTP onEmailSubmit={handleEmailSubmit} />
+          ) : (
+            <ResetPassword email={userEmail} />
+          )}
         </div>
-        <Footer></Footer>
+        <Footer />
       </div>
     </>
   );
 };
 
-export default Password;
+export default PasswordPage;
